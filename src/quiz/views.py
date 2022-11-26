6f4268613d5fc6bbe8950334a671a2269c613f00
Zhukov Oleksandr <zhukov_alex94@icloud.com>
2022-11-26 21:55:34 +0200

    quiz_hw_4

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -154,13 +154,13 @@ class ExamResultUpdateView(LoginRequiredMixin, UpdateView):
     def get(self, request, *args, **kwargs):
         uuid = kwargs.get('uuid')
         res_uuid = kwargs.get('res_uuid')
-        user = request.user
+        # user = request.user
 
-        result = Result.objects.get(
-            user=user,
-            uuid=res_uuid,
-            # exam__uuid=uuid,
-        )
+        # result = Result.objects.get(
+        #     user=user,
+        #     uuid=res_uuid,
+        #     # exam__uuid=uuid,
+        # )
 
         return HttpResponseRedirect(
             reverse(
